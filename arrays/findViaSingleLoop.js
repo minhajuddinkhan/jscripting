409cecb4622fd4c8fb67e689fdb4ec700288d83a
Minhaj <minhajuddinkhan75@gmail.com>
2017-08-01 23:14:07 +0500

    findViaSingleLoop

---
+++ arrays/findViaSingleLoop.js
@@ -0,0 +1,17 @@
+/**
+ *  Range: 1-5
+ *
+ *  Find the missing element with a single loop.
+ * */
+const array = [4,2,3,2];
+
+const desired = array.reduce((acc, number, i) => {
+
+    acc.sum += number;
+    acc.count += i+1;
+
+    return acc;
+}, {sum: 0, count: 0});
+
+const answer = desired.sum - desired.count;
+console.log(answer);
