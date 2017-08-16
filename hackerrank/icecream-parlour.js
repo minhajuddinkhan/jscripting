f1d7a86ab20b919cc0fe318727c30f7f8f1537f7
Minhaj <minhajuddin.khan@tenpearls.com>
2017-08-16 16:46:16 +0500

    initial DS

--- hackerrank/icecream-parlour.js
+++ hackerrank/icecream-parlour.js
@@ -5,3 +5,31 @@
  * https://www.hackerrank.com/challenges/icecream-parlor
  * */
 
+const input = `2
+4
+5
+1 4 5 3 2
+4
+4
+2 2 4 3`;
+
+
+
+function getTours(input) {
+  let tours = [];
+  let count = 1;
+  const splitter =  input.split('\n');
+  for(let i = 0; i < splitter[0]; i++){
+    tours[i] = {
+      pool: splitter[count++],
+      iceCream: splitter[count++],
+      varities: splitter[count++]
+    };
+  }
+  return tours;
+}
+
+let tours = getTours(input);
+console.log(tours);
+
+
