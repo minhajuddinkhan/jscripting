0bfb9c6fb03978402f8ce1a92dbe00d848e40505
Minhaj <minhajuddin.khan@tenpearls.com>
2017-08-17 15:30:32 +0500

    pti movement

--- hackerrank/icecream-parlour.js
+++ hackerrank/icecream-parlour.js
@@ -1,10 +1,10 @@
 /**
- *
- *
- * Each time Sunny and Johnny take a trip to the Ice Cream Parlor, they pool together  dollars for ice cream.
- * https://www.hackerrank.com/challenges/icecream-parlor
- * */
-
+*
+*
+* Each time Sunny and Johnny take a trip to the Ice Cream Parlor, they pool together  dollars for ice cream.
+* https://www.hackerrank.com/challenges/icecream-parlor
+*
+*/
 const input = `2
 4
 5
@@ -12,7 +12,6 @@ const input = `2
 4
 4
 2 2 4 3`;
-
 
 
 function getTours(input) {
@@ -30,6 +29,47 @@ function getTours(input) {
 }
 
 let tours = getTours(input);
-console.log(tours);
+
+tours.forEach((tour) => {
+
+  ///console.log(fetchSelections(tour.varities.split(' '), tour.pool));
+});
+
+
+
+function fetchSelections(array, sum) {
+
+
+/*
+  let hash = array.reduce((acc, elem, i) => {
+    acc[elem] = i;
+    return acc;
+  }, {});
+
+  for (let i = 0; i < array.length; i++) {
+    if (hash[sum - array[i]] != i) {
+      console.log(`${i + 1} ${ parseInt(array[hash[sum - array[i]]]) + 1}`);
+      break;
+
+    }
+  }
+*/
+}
+
+
+
+let yolo = [1, 4, 5, 3, 2];
+
+
+let res = yolo.map(() => {
+  let yolo2 = (JSON.parse(JSON.stringify(yolo)))
+  yolo2.push(yolo2[0]);
+  yolo2.splice(0,1);
+  yolo = yolo2;
+  return yolo2;
+});
+
+console.log(res);
+
 
 
